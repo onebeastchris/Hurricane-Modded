833d0bb4eeb0cae7ceb52c0d161dc1d37cc0ee9b
chris <github@onechris.mozmail.com>
2024-09-03 23:24:29 +0200

    Fix: Mixin not being remapped

--- shared/src/main/java/net/onebeastchris/hurricane/mixin/ServerPlayerInteractionManagerMixin.java
+++ shared/src/main/java/net/onebeastchris/hurricane/mixin/ServerPlayerInteractionManagerMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Slice;
 public class ServerPlayerInteractionManagerMixin {
     @Redirect(method = "handleBlockBreakAction",
             slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=Mismatch in destroy block pos: {} {}", ordinal = 0)),
-            at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V", remap = false))
+            at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"))
     private void GeyserHacks$onMismatchInDestroyBlockPos(org.slf4j.Logger logger, String format, Object arg1, Object arg2) {
         // do nothing
     }
