ee0bab37ba3e2d7320260f1fc1805a1b84b8592a
SleepyPop <81194303+SleepyPopp@users.noreply.github.com>
2026-07-09 06:32:08 -0400

    Update Config.java

--- shared/src/main/java/net/onebeastchris/hurricane/config/Config.java
+++ shared/src/main/java/net/onebeastchris/hurricane/config/Config.java
@@ -6,10 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.configurate.CommentedConfigurationNode;
 import org.spongepowered.configurate.ConfigurateException;
-import org.spongepowered.configurate.NodePath;
 import org.spongepowered.configurate.hocon.HoconConfigurationLoader;
 import org.spongepowered.configurate.transformation.ConfigurationTransformation;
-import org.spongepowered.configurate.transformation.TransformAction;
 
 @Getter
 public class Config {
@@ -23,18 +21,10 @@ public class Config {
     public static HurricaneConfiguration config;
 
     private final ConfigurationTransformation.Versioned transformer = ConfigurationTransformation.versionedBuilder()
-            .addVersion(1, zeroToOne())
+            .addVersion(1)
             .build();
 
     public Config() {
-
-        if (PlatformUtil.oldConfigPath().toFile().exists()) {
-            LOGGER.warn("""
-                    Old Hurricane config found (geyserhacks.conf).
-                    This is no longer used - please migrate to the new config (hurricane.conf)!
-                    """);
-        }
-
         final HoconConfigurationLoader loader = HoconConfigurationLoader.builder()
                 .path(PlatformUtil.configPath())
                 .defaultOptions(opts -> opts.header("Hurricane Configuration "))
@@ -67,11 +57,5 @@ public class Config {
         this.bamboo = config.collisionFixes().bamboo();
         this.pointedDripstone = config.collisionFixes().pointedDripstone();
         this.suppressWarnings = config.suppressWarnings();
-    }
-
-    private ConfigurationTransformation zeroToOne() {
-        return ConfigurationTransformation.builder()
-                .addAction(NodePath.path("item-steerable-fix"), TransformAction.remove())
-                .build();
     }
 }
